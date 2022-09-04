52ebb80871f5966550ce8e49d75949ac34e47255
Marina Galinova <m.kriklivenko@gmail.com>
2022-09-04 22:18:40 +0300

    Post

---
+++ src/main/java/Post.java
@@ -0,0 +1,24 @@
+public class Post {
+    public String name; // поле для имени
+    public String patronymic; // поле для отчетсва
+    public String surname; // поле для фамилии
+    public String phone; // поле телефонного номера
+    public String passport; // поле для паспорта
+    public Boolean subscription; // поле зарегистрирован или нет
+    public FormDate birthday; //дата рождения
+
+
+}
+//{
+//        "birthday": {
+//        "day": 13,
+//        "month": 6,
+//        "year": 1999
+//        },
+//        "name": "Иван",
+//        "passport": "4444 № 44444444",
+//        "patronymic": "Иванович",
+//        "phone": "+7 (999)-999-99-99",
+//        "surname": "Иванов",
+//        "subscription": true
+//        }
